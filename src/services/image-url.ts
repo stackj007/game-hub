640fb111aff9df72f6104a73815b95dd8d56b713
Ahmed Gamal <agamal0077@gmail.com>
2024-04-30 17:54:16 +0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,12 +1,11 @@
-const cropImageUrl = (url: string) => {
+import noImage from '../assets/no-image-placeholder.webp'
 
-  if (!url) return ''
+const cropImageUrl = (url: string) => {
+  if (!url) return noImage
 
   const target = 'media/'
   const index = url.indexOf(target) + target.length
-  return (
-    url.slice(0, index) + 'crop/600/400/' + url.slice(index)
-  )
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
 }
 
 export default cropImageUrl
